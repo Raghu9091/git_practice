8f7add8a45bfe72a22916602d52c74c66fac41f8
SanjuLaptop <raghu.b.7676@gmail.com>
2025-05-23 16:41:52 +0530

    updated the sell script exit status

---
+++ Python/datatypes.py
@@ -0,0 +1,4 @@
+lst=[10, 20 ,30 , 40, False, True, 3+4j, "raghu",[1,3,5]]
+#print(dir(lst)) #'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+print(lst.extend([2,4,5]))
+print(lst)
--- install.sh
+++ install.sh
@@ -4,5 +4,33 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "Error :: please run the script as a root user"
+    exit 1 #give 
 else
     echo "your script running succuss full"
+fi
+
+dnf list installed mysql
+if [$? -ne 0]
+then
+    echo "My sql is not installed"
+    dnf install mysql -y
+    if [$? -eq 0]
+    then
+        echo "installing my sql is succuss"
+    else
+        echo "installing  my sql is FAILURE"
+        exit 1
+    fi
+else
+    echo "My sQL is Alredy installed,nothing to do"
+
+fi
+
+# dnf install mysql -y
+# if [$? -eq 0]
+# then
+#     echo "installing my sql is succuss"
+# else
+#     echo "installing  my sql is FAILURE"
+#     exit 1
+# fi
